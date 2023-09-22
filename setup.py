43a5a9f8703cdd7ebcd5397662d222521558e079
CaptainAlready <pkapetanidis@gmail.com>
2023-09-22 13:54:12 +0300

    add init & setup files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SimpleSpecAugment',
+    version='0.0.1',
+    packages=['SimpleSpecAugment'],
+    install_requires=[
+        'numpy',
+    ],
+    python_requires=">=3.7"
+)
